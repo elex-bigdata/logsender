2fe791c1bb9b97ca348ac1fb18d6da1fab812c1f
griffinl <lq0987@126.com>
2014-04-28 14:19:58 +0800

    update

--- routes/v4.js
+++ routes/v4.js
@@ -1,18 +1,18 @@
 
-var LogManager = require("./LogManager")
+var manager = require("./LogManager")
 var saveLogManager = require("./SaveLogManager")
 
 exports.sendLog = function(req,res){
     console.time('send-log');
-    var manager = new LogManager(Date.now());
+    var ea_start_time = Date.now();
     try {
         manager.enter(req,res);
         var msg = saveLogManager.saveLog(req);
         console.log(msg);
-        manager.leave(req,res,"php",msg);
+        manager.leave(req,res,"php",msg,ea_start_time);
         console.timeEnd('send-log');
     } catch (err) {
-        manager.leave(req,res,"error",err.message)
+        manager.leave(req,res,"error",err.message,ea_start_time)
         console.error(err);
         console.timeEnd('send-log');
     }
