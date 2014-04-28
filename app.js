cf7cb624e9ccb83e6fe2350283a0e2e38a2650b6
griffinl <lq0987@126.com>
2014-04-28 14:19:22 +0800

    update

--- app.js
+++ app.js
@@ -5,79 +5,54 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cluster = require('cluster');
 
 var routes = require('./routes');
 var logSend = require('./routes/v4');
 
-if(cluster.isMaster){
+var app = express();
 
-    // Count the machine's CPUs
-    var cpuCount = require('os').cpus().length;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-    // Create a worker for each CPU
-    for (var i = 0; i < cpuCount; i += 1) {
-        cluster.fork();
-    }
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(app.router);
 
-    // Listen for dying workers
-    cluster.on('exit', function (worker) {
+app.get('/', routes.index);
+app.get('/v4/:appid/:uid', logSend.sendLog);
 
-        // Replace the dead worker,
-        // we're not sentimental
-        console.log('Worker ' + worker.id + ' died :(');
-        cluster.fork();
+/// catch 404 and forwarding to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
 
-    });
+/// error handlers
 
-}else{
-
-    var app = express();
-
-    // view engine setup
-    app.set('views', path.join(__dirname, 'views'));
-    app.set('view engine', 'jade');
-
-    app.use(favicon());
-    app.use(logger('dev'));
-    app.use(bodyParser.json());
-    //app.use(bodyParser.urlencoded());
-    //app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, 'public')));
-    app.use(app.router);
-
-    app.get('/', routes.index);
-    app.get('/v4/:appid/:uid', logSend.sendLog);
-
-    /// catch 404 and forwarding to error handler
-    app.use(function(req, res, next) {
-        var err = new Error('Not Found');
-        err.status = 404;
-        next(err);
-    });
-
-    /// error handlers
-
-    // development error handler
-    // will print stacktrace
-    if (app.get('env') === 'development') {
-        app.use(function(err, req, res, next) {
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        });
-    }
-
-    // production error handler
-    // no stacktraces leaked to user
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
     app.use(function(err, req, res, next) {
         res.render('error', {
             message: err.message,
-            error: {}
+            error: err
         });
     });
-
-
-    module.exports = app;
 }
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
