0d6710715c4b0d792e6d6e8e042c185a85519f6c
griffinl <lq0987@126.com>
2014-05-12 10:56:18 +0800

    start / restart

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -5,3 +5,11 @@ import os
 
 def rsync():
     local("rsync -avz --progress /home/hadoop/git_project_home/logsender/ %s@%s:/home/hadoop/xa/logsender/" % (env.user,env.host))
+
+def start():
+	run("pm2 start /home/hadoop/git_project_home/logsender/bin/www -i max")
+
+def restart():
+	run("pm2 dump")
+	run("pm2 kill")
+	run("pm2 resurect")
