86f4bfe8eee540c1ad277f92b2f3039878ad1f40
griffinl <lq0987@126.com>
2014-05-14 16:58:58 +0800

    v5 for test

--- bin/fabfile.py
+++ bin/fabfile.py
@@ -7,10 +7,10 @@ def deploy():
     local("rsync -avz --progress /home/hadoop/git_project_home/logsender/ %s@%s:/home/hadoop/xa/logsender/" % (env.user,env.host))
 
 def start():
-	run("pm2 start /home/hadoop/xa/logsender/bin/www -i max")
+	run("sudo pm2 start /home/hadoop/xa/logsender/bin/www -i max")
 
 def restart():
-	run("pm2 restart www")
+	run("sudo pm2 restart www")
 
 def stop():
-	run("pm2 stop www")
+	run("sudo pm2 stop www")
